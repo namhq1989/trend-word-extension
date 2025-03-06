de5553c5c5fd873abf951073e8f485effe6fa6c2
Nam HQ <namhq.1989@gmail.com>
2025-03-06 17:35:25 +0700

    feat(web): add core

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3070,
+    cors: {
+      origin: [
+        // ⚠️ SECURITY RISK: Allows any chrome-extension to access the vite server ⚠️
+        // See https://github.com/crxjs/chrome-extension-tools/issues/971 for more info
+        // I don't believe that the linked issue mentions a potential solution
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+  legacy: {
+    // ⚠️ SECURITY RISK: Allows WebSockets to connect to the vite server without a token check ⚠️
+    // See https://github.com/crxjs/chrome-extension-tools/issues/971 for more info
+    // The linked issue gives a potential fix that @crxjs/vite-plugin could implement
+    skipWebSocketTokenCheck: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        background: path.resolve(__dirname, 'scripts/background.js'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        format: 'es',
+        dir: 'dist',
+      },
+    },
+
+    sourcemap: true,
+  },
+})
