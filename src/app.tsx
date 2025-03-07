472f4bd3eeac976456d9c1e2a862cffe07a36cb6
Nam HQ <namhq.1989@gmail.com>
2025-03-07 11:44:00 +0700

    feat(web): change to ShadCN & done home ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
 import { Router } from 'react-chrome-extension-router'
 import HomeScreen from '@/resources/screens/home-screen.tsx'
+import { ThemeProvider } from '@/components/theme/theme.tsx'
 
 const App = () => {
   return (
-    <Router>
-      <HomeScreen />
-    </Router>
+    <ThemeProvider defaultTheme='dark' storageKey='ui-theme'>
+      <Router>
+        <HomeScreen />
+      </Router>
+    </ThemeProvider>
   )
 }
 
