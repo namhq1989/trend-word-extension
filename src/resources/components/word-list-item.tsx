bb318ac03cfe65cdfa89aedba1015f4c0f7c55ab
Nam Huynh <namhq1989@users.noreply.github.com>
2025-03-07 14:48:51 +0700

    feat(web): add word list ui (#4)

---
+++ src/resources/components/word-list-item.tsx
@@ -0,0 +1,51 @@
+import { Bookmark, ChevronRight } from 'lucide-react'
+import { Badge } from '@/components/ui/badge.tsx'
+import { Separator } from '@/components/ui/separator.tsx'
+
+const WordListItem = () => {
+  return (
+    <div className='flex flex-col bg-container p-4 gap-2'>
+      <div className='flex justify-between items-center'>
+        <h2 className='text-2xl text-primary font-bold cursor-pointer'>
+          emergency
+        </h2>
+        <Bookmark
+          size={24}
+          className='text-muted-foreground cursor-pointer'
+          fill='var(--primary)'
+          color='var(--primary)'
+        />
+      </div>
+      <div className='flex flex-row gap-1'>
+        <Badge variant='secondary'>intermediate</Badge>
+        <Badge variant='secondary'>noun</Badge>
+      </div>
+      <p className='text-sm mt-2'>
+        something dangerous or serious, such as an accident, that happens
+        suddenly or unexpectedly and needs fast action in order to avoid harmful
+        results
+      </p>
+      <Separator className='bg-foreground/10 my-2' />
+      <div className='flex justify-between items-center'>
+        <div className='flex flex-shrink-0 gap-2 items-center text-muted-foreground'>
+          <p>Sports</p>
+          <p>•</p>
+          <a
+            href='https://google.com.vn'
+            target='_blank'
+            className='underline underline-offset-2'
+          >
+            Source
+          </a>
+          <p>•</p>
+          <p>15/02/2025</p>
+        </div>
+        <div className='flex w-5 h-5 items-center justify-center text-muted-foreground cursor-pointer'>
+          <ChevronRight size={20} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WordListItem
---
+++ src/resources/screens/word-list-screen.tsx
@@ -0,0 +1,75 @@
+import HeaderTitle from '@/resources/components/header-title.tsx'
+import BackButton from '@/resources/components/back-button.tsx'
+import WordListItem from '@/resources/components/word-list-item.tsx'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select.tsx'
+
+const WordListScreen = () => {
+  return (
+    <div className='flex flex-col w-[400px] min-h-[600px] scrollbar-hide'>
+      <div className='flex w-full flex-row justify-between p-4 border-b-[1px]'>
+        <BackButton />
+        <HeaderTitle title='Words' />
+      </div>
+      <div className='flex flex-col p-4 gap-4'>
+        <div className='flex gap-2'>
+          <CategoryFilter />
+          <BookmarkFilter />
+        </div>
+        <WordListItem />
+        <WordListItem />
+        <WordListItem />
+        <WordListItem />
+        <WordListItem />
+        <WordListItem />
+      </div>
+    </div>
+  )
+}
+
+const CategoryFilter = () => {
+  return (
+    <Select>
+      <SelectTrigger className='w-[160px]'>
+        <SelectValue placeholder='Category' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value='politics'>Politics</SelectItem>
+          <SelectItem value='technology'>Technology</SelectItem>
+          <SelectItem value='business'>Business</SelectItem>
+          <SelectItem value='science'>Science</SelectItem>
+          <SelectItem value='health'>Health</SelectItem>
+          <SelectItem value='sports'>Sports</SelectItem>
+          <SelectItem value='entertainment'>Entertainment</SelectItem>
+          <SelectItem value='world'>World News</SelectItem>
+          <SelectItem value='education'>Education</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
+const BookmarkFilter = () => {
+  return (
+    <Select>
+      <SelectTrigger className='w-[140px]'>
+        <SelectValue placeholder='Bookmark' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value='politics'>All</SelectItem>
+          <SelectItem value='technology'>Bookmarked</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default WordListScreen
